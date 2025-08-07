0041df979582d53f38694cb936388a2229c01123
mohanlal.shanmuganathi <mohanlal.shanmuganathi@healthec.com>
2025-08-07 14:37:33 +0530

    Switch to static HTML deployment for Vercel

--- frontend/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Performance Management System</h1>
-      <p>Test page - if you see this, Next.js is working!</p>
-    </div>
-  );
-}
